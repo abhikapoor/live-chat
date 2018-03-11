8b74067fc60266fe3369ea27eeca8c08c943be5b
RecordEvolution <recordevolution@recordetionsmbp.localdomain>
2018-03-11 22:10:24 +0100

    small message

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var socket = require('socket.io');
+const PORT = process.env.PORT || 5000
 
 // App setup
 var app = express();
-var server = app.listen(5000, function() {
-    console.log('listening for requests on port 4000,');
+var server = app.listen(PORT, function() {
+    console.log('listening for requests on port ', PORT);
 });
 
 // Static files
