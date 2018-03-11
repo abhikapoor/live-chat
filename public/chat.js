aef1a459ade4a8f9795b5f1f9568abb848d6524c
RecordEvolution <recordevolution@recordetionsmbp.localdomain>
2018-03-11 22:24:45 +0100

    small message

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:5000');
+var socket = io.connect('https://live-chat-ak.herokuapp.com/');
 
 // Query DOM
 var message = document.getElementById('message'),
